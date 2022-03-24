cfec97fff20df17dc002aab5f74fb494b157bff3
Abdullah AL Habib <aalhabib001@gmail.com>
2022-03-25 02:09:55 +0600

    Updates

---
+++ precache-manifest.bbc192375bce94ffc84b3256180b1791.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4642a60c62dd0fc07b251d64ca27756",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949558723d3ae6991b88",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "b4eefdda365fa0193694",
+    "url": "/static/css/4.ea3dbbf2.chunk.css"
+  },
+  {
+    "revision": "595779f964911bfaaaa5",
+    "url": "/static/css/main.d43c34f1.chunk.css"
+  },
+  {
+    "revision": "e030c3e87a34ec543da6",
+    "url": "/static/js/2.82163ba5.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.82163ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949558723d3ae6991b88",
+    "url": "/static/js/3.598e90d3.chunk.js"
+  },
+  {
+    "revision": "b4eefdda365fa0193694",
+    "url": "/static/js/4.1a84cf6a.chunk.js"
+  },
+  {
+    "revision": "595779f964911bfaaaa5",
+    "url": "/static/js/main.628d8891.chunk.js"
+  },
+  {
+    "revision": "dbd5cf0a52f7abc3b60e",
+    "url": "/static/js/runtime-main.8ce704a4.js"
+  },
+  {
+    "revision": "a03d2ab779a458f9404fee64c16afeeb",
+    "url": "/static/media/11.a03d2ab7.PNG"
+  },
+  {
+    "revision": "fc45c04b63dee30fead7844150e7d551",
+    "url": "/static/media/12.fc45c04b.PNG"
+  },
+  {
+    "revision": "6680dbf6a2e5f20996329bd77a5553e2",
+    "url": "/static/media/13.6680dbf6.PNG"
+  },
+  {
+    "revision": "0d33935fd6bc8eebce30e1ff690efd2b",
+    "url": "/static/media/14.0d33935f.PNG"
+  },
+  {
+    "revision": "98e4f7384d8211041b7ab9a89137bd7c",
+    "url": "/static/media/171.98e4f738.PNG"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "f2b31e420137d7256301e809025d7b74",
+    "url": "/static/media/ac1.f2b31e42.jpg"
+  },
+  {
+    "revision": "0a0eda6f07441b9f58ebd7477c8cd014",
+    "url": "/static/media/ac2.0a0eda6f.png"
+  },
+  {
+    "revision": "41818ad42bcfbc8e20d9b196679e5e92",
+    "url": "/static/media/ac4.41818ad4.jpg"
+  },
+  {
+    "revision": "1eba6427da8c113b2844f51a9ae01773",
+    "url": "/static/media/diulogo.1eba6427.png"
+  },
+  {
+    "revision": "04765ce7d66350fca3cf995ba2ea5540",
+    "url": "/static/media/ibcs-logo.04765ce7.png"
+  },
+  {
+    "revision": "8eeacc2917061f2b7932e33434f9a6e2",
+    "url": "/static/media/profile.8eeacc29.jpg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "830277865be0d305f160aa8cb952fc15",
+    "url": "/static/media/undraw_Web_developer_re_h7ie.83027786.svg"
+  },
+  {
+    "revision": "46d9f22baccdf7718ee608f0b5503c6c",
+    "url": "/static/media/undraw_coding_6mjf.46d9f22b.svg"
+  },
+  {
+    "revision": "64fb8a6c031e021a72de8ea970588908",
+    "url": "/static/media/undraw_contact_us_15o2.64fb8a6c.svg"
+  }
+]);
